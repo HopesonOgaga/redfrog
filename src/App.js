1d655506538eaba173b30c4cacfe43a86708cc6e
hopeson <hopesonogaga@gmail.com>
2025-04-30 17:00:31 +0100

    klore section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Header from "./components/navagation/nav";
+import Lore from "./pages/lore";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes >
          <Route path ="/" element = {<Home></Home>}></Route>
          <Route path="nav" element={<Header></Header>}></Route>
+         <Route path="lore" element={<Lore></Lore>}></Route>
       </Routes>
     </BrowserRouter>
   );
